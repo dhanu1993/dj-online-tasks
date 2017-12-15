562507cfe18f95c4aa89f0850e72f0488be1eb69
Dhanunjaya Panchireddy <dhanunjaya.panchireddy@gmail.com>
2017-12-15 15:38:49 +0530

    update

--- project/_config.py
+++ project/_config.py
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
 CSRF_ENABLED = True
-SECRET_KEY = 'my_precious'
+SECRET_KEY = 'w\xfb\x80,\x8e\xf7\x92\xe6\xdb\x89\xed\x91[\xf2\xd8s\xd9V\xcc\xe2^\xb1\xa1\xd0'
 DEBUG = False
 
 # define the full path for the database
